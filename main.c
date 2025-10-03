5adb7b7414b4e74dfa311879caf7694513d5028a
Takowski <93830775+Takowski@users.noreply.github.com>
2025-10-03 21:50:34 +0200

    Add functions for generating, printing, and calculating differences of integer arrays

--- main.c
+++ main.c
@@ -6,13 +6,16 @@ int genNumber();
 
 int genTab(int sizeTab);
 
-int main(void) {
+int printTab(*tab)
+
+int main (void) {
     srand(time(NULL));
-    int sizeTab = 10;
 
-
-    genNumber();
-
+    int sizeTab1 = 10;
+    int sizeTab2 = 10;
+    int arr1 = genTab(sizeTab1);
+    int arr2 = genTab(sizeTab2);
+    int tabCounter=0;
 
     return 0;
 }
@@ -23,9 +26,32 @@ int genNumber() {
 }
 
 int genTab(int sizeTab) {
-    int *tab = (int*)malloc(sizeTab * sizeof(int));
+    int *tab = (int *) malloc(sizeTab * sizeof(int));
     for (int i = 0; i < sizeTab; i++) {
         tab[i] = genNumber();
+        printf("tab[%d] = %d\n", i, tab[i]);
     }
     return *tab;
+}
+
+int* diffTab(int *tab1, int *tab2, int sizeTab1, int sizeTab2) {
+    int minsize= sizeTab1>sizeTab2 ? sizeTab1 : sizeTab2;
+    int *result =NULL;
+    int count=0;
+for (int i = 0; i < minsize; i++) {
+    if (tab1[i] > tab2[i]) {
+        int diff = tab1[i] - tab2[i];
+result =(int*)realloc(result, sizeof(int)*(count+1));
+        result[count++] = diff;
+    }
+
+}
+    return result;
+}
+
+void printTab(int *tab, int sizeTab) {
+    for (int i = 0; i < sizeTab; i++) {
+        printf("%d,", tab[i]);
+    }
+    printf("\n");
 }
